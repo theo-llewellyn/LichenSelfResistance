f1c63a6962d12d26d80e8451dadce9833be26d1c
theo-llewellyn <51782438+theo-llewellyn@users.noreply.github.com>
2025-07-29 13:58:28 +0100

    Update extract_50_orthogroups.sh

--- phylogenomics/extract_50_orthogroups.sh
+++ phylogenomics/extract_50_orthogroups.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+#CHANGE TAXON $NUMBER IF 79 taxon tree or 117 taxon tree
+NUMBER=79
 mkdir Single_Copy_Orthologues_50percent
 
-cat Orthologues_Leca117T_50p.txt | while read line; do cp Orthogroup_Sequences/${line}.fa Single_Copy_Orthologues_50percent; done
+cat Orthologues_Leca${NUMBER}T_50p.txt | while read line; do cp Orthogroup_Sequences/${line}.fa Single_Copy_Orthologues_50percent; done
 
