3d13a0e95faad85c0c258a216b28c30c514119f8
theo-llewellyn <51782438+theo-llewellyn@users.noreply.github.com>
2025-07-29 13:55:48 +0100

    Update iqtree.sh

--- phylogenomics/iqtree.sh
+++ phylogenomics/iqtree.sh
@@ -4,12 +4,14 @@
 module load anaconda3/personal
 source activate iqtree-env 
 
+#CHANGE TAXON $NUMBER IF 79 taxon tree or 117 taxon tree
+NUMBER=${79}
 
-mkdir /rds/general/project/theollewellynproject/live/IQTree/IQTree_Leca117T_50p_tAL
-cd /rds/general/project/theollewellynproject/live/IQTree/IQTree_Leca117T_50p_tAL
+mkdir /rds/general/project/theollewellynproject/live/IQTree/IQTree_Leca${NUMBER}T_50p_tAL
+cd /rds/general/project/theollewellynproject/live/IQTree/IQTree_Leca${NUMBER}T_50p_tAL
 
 #make concatenated species tree
-iqtree2 -p /rds/general/user/tbl19/home/Leca117T_msa_tAl \
+iqtree2 -p /rds/general/user/tbl19/home/Leca${NUMBER}T_msa_tAl \
  --prefix concat_OF_tAl \
  -B 1000 \
  -T 32
